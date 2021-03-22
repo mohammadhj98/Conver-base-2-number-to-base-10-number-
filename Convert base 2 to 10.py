9463e8cfda69a83e9c3e2a849fafc2cd0be35cb5
Mohammad <72628758+mohammadhj98@users.noreply.github.com>
2021-03-22 17:23:10 +0430

    Update Convert base 2 to 10.py

--- Convert base 2 to 10.py
+++ Convert base 2 to 10.py
@@ -1,12 +1,7 @@
 #mohammad haje forosh
 
 #Conver base 2 number to base 10 number!
-#---------------------------------
-# binary_string=input("Enter a number :")
-# x=(int(binary_string,2))
-# print(x)
 
-#----------------------------------------
 Number = int(input("Enter a Binary number:"))
 
 dec_value = 0
@@ -22,36 +17,4 @@ while temp:
     base = base * 2
 print("Decimal number of",Number,"is : ",dec_value)
 
-#---------------------------------------------
-
-# B=input("Enter a binary String: ")
-# N=0
-# L=len(B)
-# sign=1
-# while(sign):
-#     for i in range(L):
-#         x=int(B[i])
-#         if x<0 or x>1:
-#             B=input("Enter a correct Binary String: ")
-#             L=len(B)
-#             break
-#         else:
-#             sign=0
-## for i in range(L):
-#     x=int(B[i])
-#     N=N+(x*pow(2,L-i-1))
-# print("Number in Base 10 is: %d"%N)
-
-
-
-
-
-
-
-
-#Number = input("Enter a number")   
-#s = int(Number, 2)   
-#print("Binary",Number,"is:",s) 
-
-##################
 
